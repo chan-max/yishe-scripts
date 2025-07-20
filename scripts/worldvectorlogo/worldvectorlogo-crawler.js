ea65edf15b89497160c753edda625a52f7225aad
chan-max <jackieontheway666@gmail.com>
2025-07-20 09:20:58 +0800

    dev

--- scripts/worldvectorlogo/worldvectorlogo-crawler.js
+++ scripts/worldvectorlogo/worldvectorlogo-crawler.js
@@ -3,7 +3,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-07-19 23:13:40
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-07-20 06:54:12
+ * @LastEditTime: 2025-07-20 07:56:09
  * @FilePath: /design-server/Users/jackie/workspace/yishe-scripts/scripts/worldvectorlogo-crawler.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
